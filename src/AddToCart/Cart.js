bce754f9676b54c0d427274785672b2000aa15e0
SanjuXepiar <sanjusarkar238@gmail.com>
2023-10-26 18:09:13 +0530

    Update Cart.js

--- src/AddToCart/Cart.js
+++ src/AddToCart/Cart.js
@@ -1,34 +1,33 @@
-import React from "react";
-
 const Cart = ({ cart, removeItem }) => {
   return (
     <section>
       <h1>Cart List</h1>
-      <p> Total items selected : {cart.length}</p>
+      <p> Total items selected : {cart && cart.length}</p>
       <div className="cartList">
-        {cart.map((items) => {
-          let { img, name, id, price, quantity } = items;
-          return (
-            <div className="contents" key={id}>
-              <div className="contents-img">
-                <img
-                  src={img}
-                  alt={name}
-                  style={{
-                    width: "8rem",
-                    height: "5rem",
-                  }}
-                />
+        {cart &&
+          cart.map((items) => {
+            let { img, name, id, price, quantity } = items;
+            return (
+              <div className="contents" key={id}>
+                <div className="contents-img">
+                  <img
+                    src={img}
+                    alt={name}
+                    style={{
+                      width: "8rem",
+                      height: "5rem",
+                    }}
+                  />
+                </div>
+                <div className="contents-desc">
+                  <p>{name}</p>
+                  <button onClick={() => removeItem(id)}>Remove</button>
+                  <p>Rs: {price}</p>
+                </div>
+                <p style={{ fontWeight: "500" }}>Quantity:{quantity}</p>
               </div>
-              <div className="contents-desc">
-                <p>{name}</p>
-                <button onClick={() => removeItem(id)}>Remove</button>
-                <p>Rs: {price}</p>
-              </div>
-              <p style={{ fontWeight: "500" }}>Quantity:{quantity}</p>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </section>
   );

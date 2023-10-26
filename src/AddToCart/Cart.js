955b148ba1bdae45495e1dbcf56dd770ff375a3e
SanjuXepiar <sanjusarkar238@gmail.com>
2023-10-26 18:11:34 +0530

    Update Cart.js

--- src/AddToCart/Cart.js
+++ src/AddToCart/Cart.js
@@ -2,32 +2,31 @@ const Cart = ({ cart, removeItem }) => {
   return (
     <section>
       <h1>Cart List</h1>
-      <p> Total items selected : {cart && cart.length}</p>
+      <p> Total items selected : {cart.length}</p>
       <div className="cartList">
-        {cart &&
-          cart.map((items) => {
-            let { img, name, id, price, quantity } = items;
-            return (
-              <div className="contents" key={id}>
-                <div className="contents-img">
-                  <img
-                    src={img}
-                    alt={name}
-                    style={{
-                      width: "8rem",
-                      height: "5rem",
-                    }}
-                  />
-                </div>
-                <div className="contents-desc">
-                  <p>{name}</p>
-                  <button onClick={() => removeItem(id)}>Remove</button>
-                  <p>Rs: {price}</p>
-                </div>
-                <p style={{ fontWeight: "500" }}>Quantity:{quantity}</p>
+        {cart.map((items) => {
+          let { img, name, id, price, quantity } = items;
+          return (
+            <div className="contents" key={id}>
+              <div className="contents-img">
+                <img
+                  src={img}
+                  alt={name}
+                  style={{
+                    width: "8rem",
+                    height: "5rem",
+                  }}
+                />
               </div>
-            );
-          })}
+              <div className="contents-desc">
+                <p>{name}</p>
+                <button onClick={() => removeItem(id)}>Remove</button>
+                <p>Rs: {price}</p>
+              </div>
+              <p style={{ fontWeight: "500" }}>Quantity:{quantity}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
